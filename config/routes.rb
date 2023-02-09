7c63f38f6308bbf4e06be3c3ea2cab386f621eda
victoriapham1 <vtp283@tamu.edu>
2023-02-09 09:06:09 -0600

    initial user controller & view

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
       get :delete
     end
   end
+  
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/schema.rb
+++ db/schema.rb
@@ -54,7 +54,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_08_203322) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", primary_key: "user_id", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
+    t.integer "uin"
     t.string "first_name"
     t.string "last_name"
     t.string "email"
