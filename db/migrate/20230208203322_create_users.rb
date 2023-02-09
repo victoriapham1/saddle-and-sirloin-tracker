009913232c2315974f37eaaf70700301dc3a99c9
root <root@localhost>
2023-02-09 14:17:44 +0000

    Updated users table to generate default PK and added uin as attribute

--- db/migrate/20230208203322_create_users.rb
+++ db/migrate/20230208203322_create_users.rb
@@ -1,6 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
-    create_table :users, :primary_key =>:user_id do |t|
+    create_table :users do |t|
+      t.integer :uin
       t.string :first_name
       t.string :last_name
       t.string :email
