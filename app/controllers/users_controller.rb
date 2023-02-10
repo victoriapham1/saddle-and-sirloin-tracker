fca8f72989fe3198230b238c27f88339c43abee8
victoriapham1 <vtp283@tamu.edu>
2023-02-09 23:14:16 -0600

    allow for sorting user table
    sorted by uin or first/last name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,21 @@
 class UsersController < ApplicationController
+  helper_method :sort_column, :sort_direction
   def index
-    @users = User.all
+    @users = User.order(sort_column + " " + sort_direction)
   end
 
   def new
   end
 
   def edit
+  end
+
+  private
+  def sort_column
+    User.column_names.include?(params[:sort]) ? params[:sort] : "first_name"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 end
