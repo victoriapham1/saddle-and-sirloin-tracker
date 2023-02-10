83550a298edbbbccb1c26fd3556f3bc85ec2b601
Evan Burriola <evanburriola12@tamu.edu>
2023-02-10 13:08:21 -0600

    Functional OATH and basic announcement page(NO CRUD!) and calendar page.

---
+++ app/controllers/announcements_controller.rb
@@ -0,0 +1,20 @@
+class AnnouncementsController < ApplicationController
+  def index
+    @announcements = Announcement.all
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def delete
+  end
+
+  def calendar
+  end
+end
