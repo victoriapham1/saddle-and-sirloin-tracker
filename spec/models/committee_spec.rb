e24385db8a9af43b1f708dddc94b4d4ce6866b00
victoriapham1 <vtp283@tamu.edu>
2023-02-19 14:07:51 -0600

    validation tests for committees
    update committee model to include all attributes

--- spec/models/committee_spec.rb
+++ spec/models/committee_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Committee, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validation tests' do
+
+    it 'ensures committee description must be present' do
+      com = Committee.new(committee_name: "IM Committee").save
+      expect(com).to eq(false)
+    end
+
+    it 'ensures committee name must be present' do
+      com = Committee.new(description: "Organize team sport competitions").save
+      expect(com).to eq(false)
+    end
+
+    it 'ensures all attributes must be present' do
+      com = Committee.new(committee_name: "IM Committee", description: "Organize sport teams.").save
+      expect(com).to eq(true)
+    end
+
+  end
 end
