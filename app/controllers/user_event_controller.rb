5dff6bf5b3bb97948122cdb607d7265f0a0bfbc4
Bernardo Glz de Castilla <bernardogdc@tamu.edu>
2023-03-05 15:40:31 -0600

    Security measures for user-event field
    added measures that prevents users from trying to record attendance for inputs less than 9 ints (UIN length). Gives error message if the UIN entered doesn't correspond to any user in the database.

--- app/controllers/user_event_controller.rb
+++ app/controllers/user_event_controller.rb
@@ -4,16 +4,22 @@ class UserEventController < ApplicationController
 
   def create
     @user_event = UserEvent.new(user_event_params)
+    #get user id from uin
     @user = User.where(uin: @user_event.user_id).first
-    @user_event.user_id = @user.id
-    @event = Event.where(id: @user_event.event_id)
-    respond_to do |format|
-      if @user_event.save
-        format.html { redirect_to event_url(@event), notice: "Attendance recorded" }
-        format.json { render :show, status: :created, location: @event }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
+    if @user.nil?
+      redirect_to event_url(@user_event.event_id), notice: "Member not found"
+    else
+      @user_event.user_id = @user.id
+      @event = Event.where(id: @user_event.event_id)
+
+      respond_to do |format|
+        if @user_event.save
+          format.html { redirect_to event_url(@event), notice: "Attendance recorded" }
+          format.json { render :show, status: :created, location: @event }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @event.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
@@ -24,4 +30,5 @@ class UserEventController < ApplicationController
   def user_event_params
     params.require(:user_event).permit(:user_id, :event_id, :attendance)
   end
+
 end
