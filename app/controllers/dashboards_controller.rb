c4ab86576564ba7b7b6ceb3263ab390782524349
Evan Burriola <evanburriola12@tamu.edu>
2023-03-24 01:55:22 -0500

    Created basic announcement feed on the dashboard page. Also fixed Cowboy hat not rendering on ID'd pages.

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,6 +3,7 @@ class DashboardsController < ApplicationController
   def show
     # Redirect the user to the create a profile page if
     # the Oauth admin email doesn't exist as a User!
+    @announcements = Announcement.all
     redirect_to(controller: 'users', action: 'new') if User.find_by(email: current_admin.email).nil?
   end
 end
