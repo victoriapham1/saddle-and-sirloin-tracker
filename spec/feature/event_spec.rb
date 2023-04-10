6f535800e13e5a57b7ad92a4446a4891c702d346
Bernardo Glz de Castilla <bernardogdc@tamu.edu>
2023-04-09 21:33:30 -0500

    Fixing for events tests

--- spec/feature/event_spec.rb
+++ spec/feature/event_spec.rb
@@ -6,24 +6,27 @@ RSpec.describe('Event', type: :feature) do
 
    it "searches by name" do
     event = Event.new(name: "Tailgate", event_type: 3, date: '12/12/2023',
-        description: 'cookout where you can meet fellow members.', start_time: "Sat, 01 Jan 2000 06:00:00.000000000 CST -06:00",
-        end_time: start_time: "Sat, 01 Jan 2000 06:00:00.000000000 CST -08:00").save
+        description: 'cookout where you can meet fellow members.', start_time: Time.now,
+        end_time: Time.now + 2.hours).save
         events = Event.search("tail",'')
         expect(events[0].name).to(eq("Tailgate"))
    end
 
    it "searches by category" do
     event = Event.new(name: "Tailgate", event_type: 3, date: '12/12/2023',
-        description: 'cookout where you can meet fellow members.', start_time: "Sat, 01 Jan 2000 06:00:00.000000000 CST -06:00", end_time: start_time: "Sat, 01 Jan 2000 06:00:00.000000000 CST -08:00").save
+        description: 'cookout where you can meet fellow members.', start_time: Time.now, 
+        end_time: Time.now + 2.hours).save
     events = Event.search('','Social')
     expect(events[0].event_type).to(eq(3))
    end
 
    it "returns all events if no event has searched name (w/o category)" do
     event = Event.new(name: "Tailgate", event_type: 3, date: '12/12/2023',
-        description: 'cookout where you can meet fellow members.').save
+        description: 'cookout where you can meet fellow members.', start_time: Time.now,
+        end_time: Time.now + 2.hours).save
     event2 = Event.new(name: "cookout", event_type: 2, date: '08/12/2023',
-        description: 'cookout where you can meet fellow members.').save
+        description: 'cookout where you can meet fellow members.', start_time: Time.now,
+        end_time: Time.now + 2.hours).save
     all_events = Event.all
     events = Event.search("xyz", '')
 
@@ -32,9 +35,11 @@ RSpec.describe('Event', type: :feature) do
 
    it "returns all events if no event has searched name (w/ category)" do
     event = Event.new(name: "Tailgate", event_type: 3, date: '12/12/2023',
-        description: 'cookout where you can meet fellow members.').save
+        description: 'cookout where you can meet fellow members.', start_time: Time.now,
+        end_time: Time.now + 2.hours).save
     event2 = Event.new(name: "cookout", event_type: 2, date: '08/12/2023',
-        description: 'cookout where you can meet fellow members.').save
+        description: 'cookout where you can meet fellow members.', start_time: Time.now,
+        end_time: Time.now + 2.hours).save
     all_events = Event.all
     events = Event.search("xyz", 'Social')
 
