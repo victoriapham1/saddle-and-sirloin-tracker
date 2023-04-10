685a6169937c1185b0231c1509ea2100c1102b67
Bernardo Glz de Castilla <bernardogdc@tamu.edu>
2023-04-09 21:17:17 -0500

    adding start_end time to tests

--- spec/feature/event_spec.rb
+++ spec/feature/event_spec.rb
@@ -5,15 +5,16 @@ RSpec.describe('Event', type: :feature) do
    login
 
    it "searches by name" do
-        event = Event.new(name: "Tailgate", event_type: 3, date: '12/12/2023',
-                        description: 'cookout where you can meet fellow members.').save
+    event = Event.new(name: "Tailgate", event_type: 3, date: '12/12/2023',
+        description: 'cookout where you can meet fellow members.', start_time: "Sat, 01 Jan 2000 06:00:00.000000000 CST -06:00",
+        end_time: start_time: "Sat, 01 Jan 2000 06:00:00.000000000 CST -08:00").save
         events = Event.search("tail",'')
         expect(events[0].name).to(eq("Tailgate"))
    end
 
    it "searches by category" do
     event = Event.new(name: "Tailgate", event_type: 3, date: '12/12/2023',
-        description: 'cookout where you can meet fellow members.').save
+        description: 'cookout where you can meet fellow members.', start_time: "Sat, 01 Jan 2000 06:00:00.000000000 CST -06:00", end_time: start_time: "Sat, 01 Jan 2000 06:00:00.000000000 CST -08:00").save
     events = Event.search('','Social')
     expect(events[0].event_type).to(eq(3))
    end
